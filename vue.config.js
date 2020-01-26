bc1f47792bc9aca4faf0f55c13a0d0bfd3cfde72
suguru_imanaga <suguru_imanaga@anothervisions.net>
2020-01-27 03:40:43 +0900

    Use html-webpack-inline-source-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const HtmlInlineSourcePlugin = require("html-webpack-inline-source-plugin");
+
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  chainWebpack: config => {
+    // Remove preload-webpack-plugin
+    config.plugins.delete("preload");
+    config.plugins.delete("prefetch");
+
+    // Use html-webpack-inline-source-plugin
+    config.plugin("html-inline-source").use(HtmlInlineSourcePlugin);
+
+    // Modify html-webpack-plugin
+    config.plugin("html").tap(args => {
+      args[0].inlineSource = ".(js|css)$";
+      return args;
+    });
+  }
 };
