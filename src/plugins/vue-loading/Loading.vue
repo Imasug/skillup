afbd51e81efd22ebcd45ed7ffdd169a27b8a3c01
suguru.imanaga <suguru.imanaga@ctc-g.co.jp>
2020-03-30 16:40:20 +0900

    Add loading plugin

---
+++ src/plugins/vue-loading/Loading.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-overlay :value="overlay" color="transparent">
+    <v-img src="./loading.svg" width="60px"></v-img>
+  </v-overlay>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Loading extends Vue {
+  overlay: boolean = false;
+
+  show(): void {
+    this.overlay = true;
+  }
+  hide(): void {
+    this.overlay = false;
+  }
+}
+</script>
+
+<style scoped>
+#loading {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+}
+</style>
---
+++ src/plugins/vue-loading/index.ts
@@ -0,0 +1,17 @@
+import _Vue from "vue";
+import Loading from "./Loading.vue";
+
+const node = document.createElement("div");
+document.body.appendChild(node);
+const vm = new Loading();
+const component = vm.$mount(node);
+
+export default class VueLoading {
+    static install(Vue: typeof _Vue) {
+        Vue.prototype.$loading = component;
+    }
+}
+
+export const loading = component;
+
+_Vue.use(VueLoading);
