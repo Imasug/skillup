fcc3c4712f2b53b06864384e99ac4bc04653b80c
suguru_imanaga <suguru_imanaga@anothervisions.net>
2020-03-30 18:59:03 +0900

    Fix package.json

--- src/plugins/vue-loading/Loading.vue
+++ src/plugins/vue-loading/Loading.vue
@@ -19,14 +19,3 @@ export default class Loading extends Vue {
   }
 }
 </script>
-
-<style scoped>
-#loading {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 9999;
-}
-</style>
